8c32b03a4de900fd3590e08977f88acdddd73761
emily.dokken <emily.dokken@instrument.com>
2024-01-10 15:14:26 -0600

    add abouut page

---
+++ src/views/AboutView.vue
@@ -0,0 +1,22 @@
+<script setup>
+</script>
+
+<template>
+  <div class="about">
+    <p class="p2">
+      <span class="p2--bold">Frontend Web Developer</span> with a knack for
+      <span class="p2--bold">JavaScript</span> and <span class="p2--bold">React</span> who combines
+      coding with a strong sense of design. She stands out for her ability to
+      <span class="p2--bold">merge creativity with functionality</span> in collaborative
+      environments.
+    </p>
+  </div>
+</template>
+
+<style lang="scss">
+  .about {
+    margin: 25% auto;
+    width: 75%;
+    text-align: center;
+  }
+</style>
