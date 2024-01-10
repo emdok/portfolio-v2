08c624fa2915270c4e196bab8d252be36947f5ef
emily.dokken <emily.dokken@instrument.com>
2024-01-09 21:06:16 -0600

    add lexend font face

---
+++ src/components/Hero.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="title">
+    <h1>Emily Dokken</h1>
+  </div>
+  <div class="nav">
+    <ul class="nav__list">
+      <li class="nav__list-item">
+        Work
+      </li>
+      <li class="nav__list-item">
+        Technology
+      </li>
+      <li class="nav__list-item">
+        Contact
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
