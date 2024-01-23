e2c1f5a0147576d4d62fb6966c797c6bdd2069db
emily.dokken <emily.dokken@instrument.com>
2024-01-23 17:45:06 -0600

    add work card front images

--- src/components/WorkCard.vue
+++ src/components/WorkCard.vue
@@ -1,20 +1,31 @@
 <script setup>
+import { defineProps } from 'vue'
 
+const props = defineProps({
+  item: Object
+})
 </script>
 
 <template>
   <div class="work-card">
-
+    <img v-if="item.images && item.images.length > 0" :src="item.images[0]" />
   </div>
 </template>
 
 <style lang="scss">
-  .work-card {
-    background: $color-light-gray;
-    border: 2px solid $color-dark-gray;
-    border-radius: 10px;
-    box-shadow: -3px 3px $color-dark-gray;
-    width: 275px;
-    height: 300px;
+.work-card {
+  background: $color-light-gray;
+  border: 2px solid $color-dark-gray;
+  border-radius: 10px;
+  box-shadow: -3px 3px $color-dark-gray;
+  width: 275px;
+  height: 300px;
+  overflow: hidden;
+
+  img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
   }
+}
 </style>
