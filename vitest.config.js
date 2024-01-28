8c3b67fd75a8570eb09d9593e10b9778b7a2dbef
emily.dokken <emily.dokken@instrument.com>
2024-01-28 11:49:28 -0600

    add base

--- vitest.config.js
+++ vitest.config.js
@@ -5,6 +5,7 @@ import viteConfig from './vite.config'
 export default mergeConfig(
   viteConfig,
   defineConfig({
+    base: "/portfolio-v2",
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
